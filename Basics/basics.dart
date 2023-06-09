7c1af41273d7afd3ea9717e39d52104cfe63816e
Rajeev Krishna <lambo2572@gmail.com>
2023-06-10 00:12:48 +0530

    Created Basics Folder

--- Basics/basics.dart
+++ Basics/basics.dart
@@ -1,5 +1,25 @@
-main(){
+// this is main function through which the program executes
+
+// void type function has no return-type value
+void main(){
+  // print function
   print("Hello world");
+
+  // Variables in dart 
   var name = "Rajeev Krishna";
+  var num1 = 10;
+  var num2 = 20;
+  print(num1+num2);
   print(name);  
+
+  // array in dart
+  var flyObjects =['Jupiter', 'Earth', "Saturn"];
+  print(flyObjects);
+
+  // objects in dart 
+  var image = {
+    'tags': ['Earth'],
+    'url': 'https://en.wikipedia.org/wiki/Earth'
+  };
+  print(image);
 }
